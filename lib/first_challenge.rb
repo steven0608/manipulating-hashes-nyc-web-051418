f300810015408b9be46d2bbe0705b5200537623b
Steven Chen <ysteven.chen2@gmail.com>
2018-05-07 17:14:36 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,14 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts.each do |name,data|
-  data.each do |attribute,value|
-    if attribute == [:favorite_icecream_flavors]
-      value.select do |x| x!="strawberry"
-      end
-    end
-  end
-end
+contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
 
   #remember to return your newly altered contacts hash!
   contacts
